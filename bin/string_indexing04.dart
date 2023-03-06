303dae32f66e5e2654d056202b35b81852a8a95c
Mirjalol <jalolubaydullayev575@gmail.com>
2023-03-06 13:51:56 +0500

    string04

--- bin/string_indexing04.dart
+++ bin/string_indexing04.dart
@@ -1,11 +1,11 @@
 String func(String s) {
-  """
-    The s string variable is given. Return three characters from the beginning.
-    Args:
-        s(str): parameter
-    Returns:
-        str: answer
-    """;
+  // """
+  //   The s string variable is given. Return three characters from the beginning.
+  //   Args:
+  //       s(str): parameter
+  //   Returns:
+  //       str: answer
+  //   """
   //your code here
    String str = s[0] + s[1] + s[2];
   return str;
